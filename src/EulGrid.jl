aa40ab37d542020f1e1fe3ed4e08da1b84c80e86
Owen Lewis <owenlewis@unm.edu>
2024-01-17 13:32:58 -0700

    I've begun adding the types and constructors necessary for information on the Eulerian grid of an Immersed Boundary Simulation.
    -Owen

--- src/EulGrid.jl
+++ src/EulGrid.jl
@@ -1,8 +1,11 @@
 # Code to be loaded into IBMProject
 
 abstract type AbstractGrid end
+abstract type AbstractGridFunction end
+abstract type DifferentialOperator end
 
-struct PeriodicGrid <: AbstractGrid
+#Lets define an Eulerian Grid
+struct PeriodicEulGrid <: AbstractGrid
 	Lx::Float64
 	Ly::Float64
 	Ux::Float64
@@ -11,35 +14,63 @@ struct PeriodicGrid <: AbstractGrid
 	Ny::Int
 	dx::Float64
 	dy::Float64
+	X::Matrix{Float64}
+	Y::Matrix{Float64}
 	L::Float64
 	H::Float64
 end
 
+#Lets define scalar data on an Eulerian Grid
+mutable struct ScalarGridData <: AbstractGridFunction
+	U::Matrix{Float64}
+	grid::AbstractGrid
+
+	function ScalarGridData(ux::Matrix{Float64},mygrid::AbstractGrid)
+		if ~(size(ux) == size(mygrid.X))
+			throw(ArgumentError("Size of input does not match grid size"));
+		else
+			return new(ux,mygrid)
+		end
+	end
+end
+
+# #Lets define vector data on the Eulerian Grid
+# mutable struct VectorGridData <: AbstractGridFunction
+# 	U::Matrix{Float64}
+# 	V::Matrix{Float64}
+# 	grid::AbstractGrid
+# end
+
+#Constructors
 
 #No need to specify the redundant information
-function PeriodicGrid(Lx::Real,Ly::Real,Ux::Real,Uy::Real,Nx::Int,Ny::Int)
+function PeriodicEulGrid(Lx::T,Ly::T,Ux::T,Uy::T,Nx::Int,Ny::Int) where T<:Real
 	L = Ux - Lx;
 	H = Uy - Ly;
 	dx = L/Nx;
 	dy = H/Ny;
-	grid::PeriodicGrid = PeriodicGrid(Lx,Ly,Ux,Uy,Nx,Ny,dx,dy,L,H)
+	X = [(i-1/2)*dx + Lx for i=1:Nx, j = 1:Ny];
+	Y = [(j-1/2)*dy + Ly for i=1:Nx, j = 1:Ny];
+	grid::PeriodicGrid = PeriodicGrid(Lx,Ly,Ux,Uy,Nx,Ny,dx,dy,X,Y,L,H);
 	return grid
 end
 
 #If lower bounds are not specified, we assume they're zero
-function PeriodicGrid(Ux::Real,Uy::Real,Nx::Int,Ny::Int)
+function PeriodicEulGrid(Ux::T,Uy::T,Nx::Int,Ny::Int) where T<:Real
 	Lx = 0.0;
 	Ly = 0.0;
 	L = Ux - Lx;
 	H = Uy - Ly;
 	dx = L/Nx;
 	dy = H/Ny;
-	grid::PeriodicGrid = PeriodicGrid(Lx,Ly,Ux,Uy,Nx,Ny,dx,dy,L,H)
+	X = [(i-1/2)*dx + Lx for i=1:Nx, j = 1:Ny];
+	Y = [(j-1/2)*dy + Ly for i=1:Nx, j = 1:Ny];
+	grid::PeriodicGrid = PeriodicGrid(Lx,Ly,Ux,Uy,Nx,Ny,dx,dy,X,Y,L,H);
 	return grid
 end
 
 #If upper bounds are not specified, we assume they're unity
-function PeriodicGrid(Nx::Int,Ny::Int)
+function PeriodicEulGrid(Nx::Int,Ny::Int)
 	Lx = 0.0;
 	Ly = 0.0;
 	Ux = 1.0;
@@ -48,6 +79,27 @@ function PeriodicGrid(Nx::Int,Ny::Int)
 	H = Uy - Ly;
 	dx = L/Nx;
 	dy = H/Ny;
-	grid::PeriodicGrid = PeriodicGrid(Lx,Ly,Ux,Uy,Nx,Ny,dx,dy,L,H)
+	X = [(i-1/2)*dx + Lx for i=1:Nx, j = 1:Ny];
+	Y = [(j-1/2)*dy + Ly for i=1:Nx, j = 1:Ny];
+	grid::PeriodicEulGrid = PeriodicEulGrid(Lx,Ly,Ux,Uy,Nx,Ny,dx,dy,X,Y,L,H);
 	return grid
 end
+
+function ScalarGridData(mygrid::AbstractGrid)
+	U = zeros(mygrid.Nx,mygrid.Ny);
+	griddata::ScalarGridData = ScalarGridData(U,mygrid);
+	return griddata
+end
+
+# function ScalarGridData(ux::Matrix{T},mygrid::AbstractGrid) where T<:Real
+# 	if ~(size(ux) == size(mygrid.X))
+# 		throw(ArgumentError(ux,"Size of input does not match grid size"));
+# 	else
+# 		griddata::ScalarGridData = ScalarGridData(ux,mygrid);
+# 		return griddata
+# 	end
+# end
+
+
+
+#	dataY = zeros(mygrid.Nx,grid.Ny);
