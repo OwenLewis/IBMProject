4f8f6d088449751f9c888cfd68150cca3268b435
Owen Lewis <owenlewis@unm.edu>
2025-04-15 17:34:58 -0600

    Force Calculation
    I think I got it working. Need to run some tests but you never know....

--- src/NetworkOpers.jl
+++ src/NetworkOpers.jl
@@ -1,7 +1,8 @@
 # A collection of functions associated with immersed network data types. 
 # This is where force calculations, spread and interp, as well as 
 import HDF5
-
+import StaticArrays
+import LinearAlgebra
 
 
 # This is a simple function to read a network in from an HDF5 file
@@ -93,11 +94,50 @@ function MoveNetwork!(mymesh::LagMesh,X::Vector{Vector{T}}) where T <: Real
 end
 
 
-function ElasticForceCalc(mymesh::LagMesh,lame1::T,lame2::T) where T <: Real
-	Force = [[0.0,0.0] for i in 1:mymesh.Mv]
+function ElasticForceCalc(mymesh::LagMesh,mu::T,kappa::T) where T <: Real
+	ForceDensity = [[0.0,0.0] for i in 1:mymesh.Mv]
+	A = StaticArrays.SMatrix{2,2,Float64}(0,0,0,0)
 
-	for i = 1:mymesh.Fv
+	for i = 1:mymesh.Mf
+		# I'm indexing the points zero, one, two to correspond with Strychalski, Copos, et al
+		# I'm only doing that here. 
+		zero,one,two = mymesh.faces[i].vertices;
+		ind0 = findfirst(x -> x==zero,mymesh.vertices);
+		ind1 = findfirst(x -> x==one,mymesh.vertices);
+		ind2 = findfirst(x -> x==two,mymesh.vertices);
+
+		#Now we calculate the deformation gradient tensor
+		S = mymesh.faces[i].Smat;
+		A = mymesh.faces[i].Xmat*mymesh.faces[i].Sinv;
+		#And its associated quantities
+		Ainv = LinearAlgebra.inv(A);
+		AT = LinearAlgebra.transpose(A);
+		AinvT = LinearAlgebra.transpose(Ainv);
+		J = LinearAlgebra.det(A);
+
+		#We also need the determinant of the original matrix
+		Delta = LinearAlgebra.det(S);
+
+		EnergyGradient = mu.*(A./J - LinearAlgebra.tr(A*AT).*AinvT./(2*J)) + kappa.*(J.- 1.0).*J.*AinvT;
+
+
+		#Now we loop through the 3 points. Each has its own dA/dX
+		#Point zero
+		dAdX = StaticArrays.SVector{2,Float64}((S[2,1]-S[2,2])/Delta,(S[1,2]-S[1,1])/Delta)
+		Force = -(EnergyGradient*dAdX).*mymesh.faces[i].ref_area;
+		ForceDensity[ind0] += Force./zero.ref_area
+		#Point one
+		dAdX = StaticArrays.SVector{2,Float64}(S[2,2]/Delta,-S[1,2]/Delta)
+		Force = -(EnergyGradient*dAdX).*mymesh.faces[i].ref_area;
+		ForceDensity[ind1] += Force./one.ref_area
+		#Point two
+		dAdX = StaticArrays.SVector{2,Float64}(-S[2,1]/Delta,S[1,1]/Delta)
+		Force = -(EnergyGradient*dAdX).*mymesh.faces[i].ref_area;
+		ForceDensity[ind2] += Force./two.ref_area
 
 	end
+
+
+	return ForceDensity
 end
 
